c6e11f7349596dd3bec9d1e95534997a11e37120
Juste Vaitkeviciute <justevaitkev@gmail.com>
2025-10-27 15:11:24 +0200

    add custom hash function

--- main.cpp
+++ main.cpp
@@ -31,6 +31,31 @@ static string to_hex16(uint64_t x) {
     return ss.str();
 }
 
+string custom_hash(const string& input) {
+    uint64_t part0 = 0x1234567890abcdefULL;
+    uint64_t part1 = 0xfedcba0987654321ULL;
+    uint64_t part2 = 0x0f1e2d3c4b5a6978ULL;
+    uint64_t part3 = 0x89abcdef01234567ULL;
+
+    for (unsigned char b : input) {
+        part0 = part0 + b;
+        part1 = part1 + part0 * 3;
+        part2 = part2 + part1 + (b * 7);
+        part3 = part3 + part2 + (part0 * 2);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        part0 = (part0 ^ (part1 * 3)) * 0x4e97b2d8f3c1a5e3ULL;
+        part1 = (part1 ^ (part2 * 5)) * 0xa3d5f79c482eb16fULL;
+        part2 = (part2 ^ (part3 * 7)) * 0x5b18e4c7d92fa06eULL;
+        part3 = (part3 ^ (part0 * 9)) * 0xc74a9e21f05bd83cULL;
+    }
+
+    stringstream ss;
+    ss << hex << setfill('0') << setw(16) << part0 << setw(16) << part1 << setw(16) << part2 << setw(16) << part3;
+    return ss.str();
+}
+
 int main() {
     cout << " Supaprastintas Blockchain " << VERSION_ << endl;
     return 0;
