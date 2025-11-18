4b709c827078432f01fd4e149e7a9db75d42f526
Juste Vaitkeviciute <justevaitkev@gmail.com>
2025-11-18 02:32:55 +0200

    bitcoin library test

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <bitcoin/system.hpp>
+#include <iostream>
+
+int main() {
+    std::cout << "libbitcoin version: " 
+              << LIBBITCOIN_SYSTEM_VERSION << "\n";
+
+    // Paprastas "hello" hash pavyzdys
+    libbitcoin::data_chunk data{'h','e','l','l','o'};
+
+    auto hash = libbitcoin::bitcoin_hash(data);
+    std::cout << "SHA256(SHA256(\"hello\")) = "
+              << libbitcoin::encode_base16(hash) << "\n";
+
+    return 0;
+}
